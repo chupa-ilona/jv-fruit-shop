e82c5ae7cd88ccb47477a75f82775a7cf27c9f43
IlonaChupa <ilonachupa@gmail.com>
2026-03-24 14:53:01 +0200

    done

--- src/main/java/core/basesyntax/FileReaderServiceImpl.java
+++ src/main/java/core/basesyntax/FileReaderServiceImpl.java
@@ -12,13 +12,11 @@ public class FileReaderServiceImpl implements FileReaderService {
     public List<String> read(File file) {
         StringBuilder content = new StringBuilder();
         try (FileReader fileReader = new FileReader(file);
-             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
-
+                BufferedReader bufferedReader = new BufferedReader(fileReader)) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 content.append(line).append(System.lineSeparator());
             }
-
         } catch (IOException e) {
             throw new RuntimeException("Не вдалося прочитати файл: "
                     + file.getAbsolutePath(), e);
